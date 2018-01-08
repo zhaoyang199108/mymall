d249a796af2889c230471b05ef006a31ef939bab
zhaoyang <zhaoyang199108@163.com>
2018-01-08 14:23:14 +0800

    修改校验email

--- mymall/src/main/java/com/mymall/common/Const.java
+++ mymall/src/main/java/com/mymall/common/Const.java
@@ -7,7 +7,7 @@ package com.mymall.common;
 public class Const {
 
 	public static final String CURRENT_USER="currentUser";
-	public static final String EMAIL="eamil";
+	public static final String EMAIL="email";
 	public static final String USERNAME="username";
 	public interface Role{
 		int ROLE_CUSTOMER = 0;//普通用户
